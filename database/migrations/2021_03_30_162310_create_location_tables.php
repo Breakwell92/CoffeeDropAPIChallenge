ee85b5ae539bb5da04c3ff8dfe13cdff752a23ec
Breakwell92 <dpbreakwell@gmail.com>
2021-03-31 21:19:08 +0100

    [migration] added migration for new tables
    (cherry picked from commit fc3955265f7cf0c53c49a3ee79304d46f6eae7d8)

---
+++ database/migrations/2021_03_30_162310_create_location_tables.php
@@ -0,0 +1,62 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateLocationTables extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('locations', function (Blueprint $table) {
+            $table->id();
+            $table->string('postcode');
+            $table->float('latitude', 11, 8)->nullable();
+            $table->float('longitude', 11, 8)->nullable();
+
+            $table->timestamps();
+        });
+
+        Schema::create('location_opening_times', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('location_id');
+            $table->enum('day', ['monday', 'tuesday', 'wednesday', 'thursday', 'friday', 'saturday', 'sunday']);
+            $table->string('open_time');
+            $table->string('closed_time');
+            $table->timestamps();
+
+            $table->foreign('location_id')->references('id')->on('locations');
+        });
+
+        Schema::create('cashback_calculation_requests', function (Blueprint $table) {
+            $table->id();
+            $table->ipAddress('visitor');
+            $table->string('user_agent');
+            $table->json('request');
+            $table->integer('cashback');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('location_opening_times', function (Blueprint $table) {
+            $table->dropForeign('location_id');
+        });
+
+        Schema::dropIfExists('locations');
+        Schema::dropIfExists('location_opening_times');
+        Schema::dropIfExists('cashback_calculation_requests');
+
+    }
+}
