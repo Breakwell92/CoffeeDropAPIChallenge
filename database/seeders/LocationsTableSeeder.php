705f7f2ada9116e1f7eec250a8f4198c076ed567
Breakwell92 <dpbreakwell@gmail.com>
2021-04-01 17:19:03 +0100

    [seeder] tidied seeder for better readability
    (cherry picked from commit 84d3886c938df19230f195b3303213e2737b8c5d)

--- database/seeders/LocationsTableSeeder.php
+++ database/seeders/LocationsTableSeeder.php
@@ -3,9 +3,11 @@
 namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
-use App\Http\Controllers\LocationController;
+
 use App\Models\Location;
 use App\Models\LocationOpeningTime;
+
+use PostcodeHelper;
 
 class LocationsTableSeeder extends Seeder
 {
@@ -24,45 +26,66 @@ class LocationsTableSeeder extends Seeder
      */
     public function run()
     {
-        if(($handle = fopen($this->locationDataFile, "r")) !== FALSE ){
-            $row = 1;
 
-            while (($data = fgetcsv( $handle, 1000, ",")) !== FALSE ){
-                // Skip first line containing headers.
-                if ( $row === 1 ) {
-                    $row++;
+        if(($handle = fopen($this->locationDataFile, "r")) !== FALSE ){
+
+            $row = 1;
+            $headers = [];
+
+            while (($location_data = fgetcsv($handle, 1000, ",")) !== FALSE ){
+                if($row === 1){
+                    $row++; // Skip the first row of headers
                     continue;
                 }
 
-                $postcode = $data[0];
-                $latitude_longitude = LocationController::getLatitudeLongitude($postcode);
+                // Destructure location data for readability
+                [
+                    $postcode,
+                    $monday['open'],
+                    $tuesday['open'],
+                    $wednesday['open'],
+                    $thursday['open'],
+                    $friday['open'],
+                    $saturday['open'],
+                    $sunday['open'],
+                    $monday['closed'],
+                    $tuesday['closed'],
+                    $wednesday['closed'],
+                    $thursday['closed'],
+                    $friday['closed'],
+                    $saturday['closed'],
+                    $sunday['closed'],
+                ] = $location_data;
+
+                // Fetch latitude and longitude and store in DB
+                $latitude_longitude = PostcodeHelper::getLatitudeLongitude($postcode);
                 $location = Location::create([
                     'postcode' => $postcode,
                     'latitude' => $latitude_longitude['latitude'] ?? null,
                     'longitude' => $latitude_longitude['longitude'] ?? null
                 ]);
+                
+                // Build opening times array to loop through and store in DB.
 
                 $openingTimes = [
-                    'monday' => [$data[1], $data[8]],
-                    'tuesday' => [$data[2], $data[9]],
-                    'wednesday' => [$data[3], $data[10]],
-                    'thursday' => [$data[4], $data[11]],
-                    'friday' => [$data[5], $data[12]],
-                    'saturday' => [$data[6], $data[13]],
-                    'sunday' => [$data[7], $data[14]],
+                    'monday' => $monday,
+                    'tuesday' => $tuesday,
+                    'wednesday' => $wednesday,
+                    'thursday' => $thursday,
+                    'friday' => $friday,
+                    'saturday' => $saturday,
+                    'sunday' => $sunday,
                 ];
 
                 foreach($openingTimes as $day=>$times){
-                    if(!empty($times[0]) && !empty($times[1])){
+                    if(!empty($times['open']) && !empty($times['closed'])){
                         $location->openingTimes()->create([
                             'day' => $day,
-                            'open_time' => $times[0],
-                            'closed_time' => $times[1]
+                            'open_time' => $times['open'],
+                            'closed_time' => $times['closed']
                         ]);
                     }
                 }
-
-                $row++;
             }
         }
     }
